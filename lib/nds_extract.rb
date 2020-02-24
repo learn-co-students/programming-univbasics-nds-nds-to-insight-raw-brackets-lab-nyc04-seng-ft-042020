f23a395c3c1e4b4afb7556a1a4af9de8577665fc
Kyle B <kjburda@gmail.com>
2020-02-24 17:14:59 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,22 +1,34 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
+
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+  
+  result = Hash.new
+  
+  dir_index = 0 
+  while dir_index < nds.length do
+    movies_index = 0
+    directors_totals = 0
+    
+    while movies_index < nds[dir_index][:movies].length do
+      directors_totals += nds[dir_index][:movies][movies_index][:worldwide_gross]
+      movies_index += 1
+      
+    end
+    result.merge!(nds[dir_index][:name] => directors_totals)
+    dir_index += 1
+    
+  end
+  result
+  
 end
+
+
+# Commented out testing below; used it to familiarized myself with data structure and wrote out loop logic accordingly to solve assignment
+#pp directors_database
+#p directors_database[0][:movies][1][:worldwide_gross]
+#p directors_database[1][:name]
+# p directors_totals(directors_database)
